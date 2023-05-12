16d285a8559a64b5766980813aca003c082a2f66
Olanma1 <olanmamary2@gmail.com>
2023-05-12 19:41:52 +0200

    Update topbar.vue

--- resources/js/components/topbar.vue
+++ resources/js/components/topbar.vue
@@ -6,7 +6,6 @@
       <h1 class="navbar-brand font-bold text-primary">ToNote</h1>
         </router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
