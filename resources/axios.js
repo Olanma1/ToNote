26da0777cb433f255d9ce0253f4caeb4b4b74bc3
Obasi, Mary Olanma <olanmamary2@gmail.com>
2023-05-16 16:28:30 +0200

    Update axios.js

--- resources/axios.js
+++ resources/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "./js/store/app";
 
 const axiosClient = axios.create({
-baseURL: `https://tonote-app.herokuapp.com/api`
+baseURL: `http://tonote-app.herokuapp.com/api`
 })
 axiosClient.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${store.state.user.token}`
